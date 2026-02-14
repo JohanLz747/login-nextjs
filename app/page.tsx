0c99ff6e8e88b1d635d3a0515bb7affc40ed7adb
Johan <johan.lh2001@gmail.com>
2026-02-13 22:31:50 -0500

    mensaje

--- app/page.tsx
+++ app/page.tsx
@@ -23,13 +23,13 @@ export default function Home() {
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <Link
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-blue-600 px-5 text-white transition-colors hover:bg-blue-700 md:w-[158px]"
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-blue-600 px-5 text-white transition-colors hover:bg-blue-700 md:w-158px"
             href="/register"
           >
             Registrarse
           </Link>
           <Link
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/ px-5 transition-colors hover:border-transparent hover:bg-black  dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-158px"
             href="/login"
           >
             Iniciar Sesión
